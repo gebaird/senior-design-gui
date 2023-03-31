cb771302d33a9ddcb2339690297d5f9a761b5914
gebaird <129475864+gebaird@users.noreply.github.com>
2023-03-31 21:28:49 +0000

    test save

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
-##learning how to set up kivy app
-import kivy
-kivy.require('2.1.0') # replace with your current kivy version !
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 30 21:03:58 2023
 
-from kivy.app import App
-from kivy.uix.label import Label
+@author: grace
+"""
+
+import tkinter
+from tkinter import *
+from tkinter import ttk
+from matplotlib.figure import *
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
+                                               NavigationToolbar2Tk)
 
 
-class MyApp(App):
-  def build(self):
-        return Label(text='Hello world')
 
 
-if __name__ == '__main__':
-    MyApp().run()
+def plot ():
+    fig = Figure(figsize = (5, 5), dpi = 100)
+    y = range(10)
+    plot1 = fig.add_subplot(111)
+    plot1.plot(y)
+    
+    canvas = FigureCanvasTkAgg(fig, master = root)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+    main_graph = FigureCanvasTkAgg()
+
+root = Tk(className = "GAMIX ICP MONITOR",)
+root.geometry("500x500")
+plot_label = Label(height = 2, width = 10, text = "test plot", 
+                   background = 'magenta', pady = 1, padx = 10).pack()
+root_plot = plot()
+
+root.mainloop()
+
