9ec6317635ed1f1913784a9749b601fa53c6c804
gebaird <129475864+gebaird@users.noreply.github.com>
2023-03-31 21:30:49 +0000

    fixing geometry

--- main.py
+++ main.py
@@ -12,10 +12,8 @@ from matplotlib.figure import *
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
                                                NavigationToolbar2Tk)
 
-
-
-
-def plot ():
+def plot (): #creating a basic plot on the first page
+# will update with time-average after figuring that out
     fig = Figure(figsize = (5, 5), dpi = 100)
     y = range(10)
     plot1 = fig.add_subplot(111)
@@ -25,12 +23,34 @@ def plot ():
     canvas.draw()
     canvas.get_tk_widget().pack()
     main_graph = FigureCanvasTkAgg()
+    
+def openNewWindow():
+    indiv_readouts = Tk()
+    indiv_readouts.title("INDIVIDUAL ICP READOUTS")
+    indiv_readouts.geometry("500x500")
+    back_button = Button(indiv_readouts, text = "Main Menu", 
+                         command = mainMenu(indiv_readouts))
+    back_button.pack(anchor = 'c')
+    
+def mainMenu(currentwindow):
+    currentwindow.destroy()
+    
 
-root = Tk(className = "GAMIX ICP MONITOR",)
-root.geometry("500x500")
-plot_label = Label(height = 2, width = 10, text = "test plot", 
-                   background = 'magenta', pady = 1, padx = 10).pack()
+    
+
+root = Tk() #inidializing the first window
+root.title("GAMIX ICP MONITOR")
+root.geometry("500x500") # setting geometry
+next_button = Button(root, text = "Individual ICP Readouts", 
+                     command = openNewWindow)
+next_button.pack(anchor = 's')
+                     
+#creating a label just as a rough draft
+plot_label = Label(root, text = "test plot").pack(anchor = 'n')
+#creating actual plot
 root_plot = plot()
 
-root.mainloop()
 
+
+#creating "Next" button
+mainloop()
